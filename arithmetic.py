332d540073d89d1ee67807f5fc7b866208a5c335
Gail Eddy <86028046+geddy8543@users.noreply.github.com>
2022-07-12 14:08:28 -0500

    adds arithmetic examples

---
+++ arithmetic.py
@@ -0,0 +1,7 @@
+print(10 + 3)
+print(10 * 3)
+print(10 - 3)
+print(10 / 3)
+print(10 // 3) # divides but only returns integer not decimal places
+print(10 % 3)  # modulo returns remainder
+print(10 ** 3) # 10 to the third power
