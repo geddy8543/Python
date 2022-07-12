6b56a28895e0313663db7ac553c1990ddcacd750
Gail Eddy <86028046+geddy8543@users.noreply.github.com>
2022-07-12 13:39:53 -0500

    adds examples from tutorial

---
+++ exercises.py
@@ -0,0 +1,25 @@
+# Exercise 1
+# patient_name = 'John Smith'
+# patient_age = 20
+# is_new_patient = True
+# print(patient_name, patient_age, is_new_patient)
+
+# name = input("What is your name? ")
+# print("Hello " + name)
+
+# String conversion to integer
+# birth_year = input("Enter your birth year: ")
+# age = 2022 - int(birth_year)
+# print(age)
+
+# Exercise 2 Enter two numbers, an int and a float, and find the sum
+# first_number = input("Enter an integer: ")
+# second_number = input("Enter a decimal value: ")
+# sum = int(first_number) + float(second_number)
+# print(sum)
+
+# Another way:
+# first = float(input("First: "))
+# second = float(input("Second: "))
+# sum = first + second
+# print("Sum: "+ str(sum))
