401697dec6933885d95c15a16129b7c2841c58b7
Gail Eddy <86028046+geddy8543@users.noreply.github.com>
2022-07-12 15:23:56 -0500

    adds logic examples to arithmetic examples

--- arithmetic.py
+++ arithmetic.py
@@ -5,3 +5,13 @@ print(10 / 3)
 print(10 // 3) # divides but only returns integer not decimal places
 print(10 % 3)  # modulo returns remainder
 print(10 ** 3) # 10 to the third power
+
+x = 10 + 3 * 2   # this will equal 16, will follow order of ops
+print(x)
+
+x = 3 > 2
+print(x)   # Boolean that will return true
+
+price = 25
+print(price > 10 and price < 30) #both statements will return True
+print(price > 10 or price < 30)
