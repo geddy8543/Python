2c364bd3d879d23ffa8c979575676ef4c5be462b
Gail Eddy <86028046+geddy8543@users.noreply.github.com>
2023-10-15 10:31:06 -0500

    add repr method to the class

--- oop_pluralsight_python_course.py
+++ oop_pluralsight_python_course.py
@@ -11,10 +11,15 @@ class Employee:
     # def info(self): # we took the string from this out and put it in the def __str__ method instead
     #     print(f"{self.name} is {self.age} years old. Employee is a {self.position} with the salary of ${self.salary}")
 
-    def __str__(self):
+    def __str__(self): #output will be human readable form for str
         return f"{self.name} is {self.age} years old. Employee is a {self.position} with the salary of ${self.salary}"
-        
+
+    def __repr__(self): #output will be more formal; for developers
+        return f"Employee({self.name}, {self.age}, {self.position}, {self.salary})"
+    
 employee1 = Employee("Ji-Soo", 38, "developer", 1200)
 employee2 = Employee("Lauren", 44, "tester", 1000)
-print(employee1) 
+print(str(employee1)) #use this only when you need the str
+
+
     
