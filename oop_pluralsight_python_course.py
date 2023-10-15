7ba8def74d4651f2a3624ed7c30d6e3c4f3149ae
Gail Eddy <86028046+geddy8543@users.noreply.github.com>
2023-10-15 10:33:35 -0500

    print repr

--- oop_pluralsight_python_course.py
+++ oop_pluralsight_python_course.py
@@ -14,12 +14,14 @@ class Employee:
     def __str__(self): #output will be human readable form for str
         return f"{self.name} is {self.age} years old. Employee is a {self.position} with the salary of ${self.salary}"
 
-    def __repr__(self): #output will be more formal; for developers
+    def __repr__(self): #output will be more formal; for developers (but it will not create a new employee object)
         return f"Employee({self.name}, {self.age}, {self.position}, {self.salary})"
     
 employee1 = Employee("Ji-Soo", 38, "developer", 1200)
 employee2 = Employee("Lauren", 44, "tester", 1000)
-print(str(employee1)) #use this only when you need the str
+
+# print(str(employee1)) #use this only when you need the str
+print(repr(employee1))
 
 
     
