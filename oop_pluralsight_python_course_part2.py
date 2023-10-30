85ef48bf6ba420cbc22436e709ff97876bde6d6c
Gail Eddy <86028046+geddy8543@users.noreply.github.com>
2023-10-30 12:14:34 -0500

    create second python course folder

---
+++ oop_pluralsight_python_course_part2.py
@@ -0,0 +1,10 @@
+# After the inheritance lesson
+class Employee:
+    def __init__(self, name, age, salary):
+        self.name = name
+        self.age = age
+        self.salary = salary
+
+    def increase_salary(self, percent):
+        self.salary += self.salary * (percent/100)
+        
