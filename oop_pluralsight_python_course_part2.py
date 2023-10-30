e7e3a65fcb677f2a9170f98fab35191db309b3c5
Gail Eddy <86028046+geddy8543@users.noreply.github.com>
2023-10-30 12:56:37 -0500

    add super to code

--- oop_pluralsight_python_course_part2.py
+++ oop_pluralsight_python_course_part2.py
@@ -14,8 +14,13 @@ class Tester(Employee): # the tester subclass (child class) will inherit all of 
         print("Tests are done.") 
 
 class Developer(Employee):
+    def __init__(self, name, age, salary, framework):
+        super().__init__(name, age, salary)
+        self.framework = framework
+        
     def increase_salary(self, percent, bonus=0):
-        self.salary += self.salary * (percent/100)
+        super().increase_salary(percent)
+        #Employee.increase_salary(self, percent)
         self.salary += bonus
 
 employee1 = Tester("Lauren", 44, 1000)
