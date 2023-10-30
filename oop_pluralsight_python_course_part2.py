8908e42b5f5e281edff8b5b6a4a100f6d2744201
Gail Eddy <86028046+geddy8543@users.noreply.github.com>
2023-10-30 12:19:20 -0500

    add tester class

--- oop_pluralsight_python_course_part2.py
+++ oop_pluralsight_python_course_part2.py
@@ -1,5 +1,5 @@
 # After the inheritance lesson
-class Employee:
+class Employee:  #Employee class will serve as a Parent class in the hierarchy
     def __init__(self, name, age, salary):
         self.name = name
         self.age = age
@@ -7,4 +7,11 @@ class Employee:
 
     def increase_salary(self, percent):
         self.salary += self.salary * (percent/100)
-        
+
+class Tester(Employee): # the tester subclass (child class) will inherit all of the methods from the employee class
+    pass   
+
+employee1 = Tester("Lauren", 44, 1000)
+
+employee1.increase_salary(20)
+print(employee1.salary)
