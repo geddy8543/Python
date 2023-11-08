3b9198afb8c372f49d09f0143bc25a688b0aa259
Gail Eddy <86028046+geddy8543@users.noreply.github.com>
2023-11-08 13:15:41 -0600

    add code

--- class_attributes.py
+++ class_attributes.py
@@ -1,7 +1,18 @@
+from datetime import date
 class Employee:
     minimum_wage = 1000
 
     @classmethod
+    def change_minimum_wage(cls, new_wage):
+        if new_wage > 3000:
+            raise ValueError("Company is bankrupt")
+        cls.minimum_wage = new_wage
+
+    @classmethod  #factory function
+    def new_employee(cls, name, dob):
+        now = date.today()
+        age = now.year - dob.year - ((now.month, now.day) < (dob.month, dob.day))
+        return cls(name, age, cls.minimum_wage)
 
     def __init__(self, name, age, salary):
         self.name = name
